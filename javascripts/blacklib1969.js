d8bf93ba8dd27f565aa2d22ff5b18c50d95d6f21
Nabil Kashyap <nkashya1@swarthmore.edu>
2015-01-12 19:52:17 -0500

    debugged blacklib js, added more info link

--- javascripts/blacklib1969.js
+++ javascripts/blacklib1969.js
@@ -3,13 +3,21 @@
 
 jQuery(function(){
 
+    jQuery('div.item.record', jQuery('#soundtrack-exhibit-container'))
+        .each(function(){
+            currItem = jQuery(this).children();
+            title = currItem[0].innerText;
+            jQuery(currItem[1]).children().attr('title',title);
+            jQuery(currItem[1]).attr('href','#void');
+        });
+
     function getPlayerFromID(id){
 
         jQuery.ajax({
             url: 'api/items/' + id
         }).done(function(d){
             for(i in d.element_texts){
-                currElement = d.element_texts[i];                
+                currElement = d.element_texts[i];
                 if(currElement.element.name == 'Player'){
                     playerHTML = currElement.text;
                 }
@@ -17,18 +25,20 @@ jQuery(function(){
                     description = currElement.text;
                 }
             }
+            description += "&nbsp;&nbsp;<a href='/items/show/" + id;
+            description += "'>More Info</a>";
             jQuery('div#soundtrack-exhibit-player')
                 .html(playerHTML)
-                .append("<p>" + description);    
+                .append("<p>" + description);
         });
     }
 
-    getPlayerFromID(1263);    
+    getPlayerFromID(1263);
 
     jQuery('#soundtrack-exhibit-container .item')
         .on('click', function(d){
             itemID = jQuery(d.currentTarget)
-                .children('a').attr('href');
+                .find('h3 > a').attr('href');
             itemID = itemID.substr(itemID.lastIndexOf('/') + 1);
 
             getPlayerFromID(itemID);
