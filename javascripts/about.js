8ee754871b762996b5048b9042a5d720e4165c98
Nabil Kashyap <n@nabilk.com>
2015-01-20 16:36:13 -0500

    updated about.js to use new exhibits-json data

--- javascripts/about.js
+++ javascripts/about.js
@@ -3,14 +3,12 @@
  
 jQuery(function(){
 
-    item = "<li><a href='/'><img src='http://54.210.13.122/files/fullsize/a008acc6a3c11b1fefe5d93ab888e59a.jpg' alt='Greene001.jpg' title='Greene001.jpg'>Hello Now<p>Hello Now</p><h2>Hello Now</h2></a></li>";
-
     carousel = jQuery('#jcarousel-about ul');
 
     for(i in exhibitsJSON){
 
         exhibit = "<li><a href='" + exhibitsJSON[i].url + "'>";
-        exhibit += "<img src='http://54.210.13.122/files/square_thumbnails/";
+        exhibit += "<img src='http://54.210.13.122/fullsize/square_thumbnails/";
         exhibit += exhibitsJSON[i].thumb + "' title='" + exhibitsJSON[i].title + "'/>";
         exhibit += "<div><h3>" + exhibitsJSON[i].title + "</h3></a>";
         exhibit += "<p>" + exhibitsJSON[i].description + "</p></div></li>"
@@ -19,9 +17,8 @@ jQuery(function(){
         carousel.append(exhibit);
     }
 
-    carousel = jQuery('#jcarousel-about');
-    carousel.jcarousel({
-        animation: 'slow'
-        
+    jQuery('#jcarousel-about').jcarousel({
+        animation: 'slow';
+        vertical: true;
     });
 });
