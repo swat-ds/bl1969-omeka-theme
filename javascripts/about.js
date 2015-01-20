f929079f3e21b20a2b9ba15e0bdbad14410b0845
Nabil Kashyap <n@nabilk.com>
2015-01-19 23:43:02 -0500

    about js tester

--- javascripts/about.js
+++ javascripts/about.js
@@ -7,28 +7,17 @@ jQuery(function(){
         
     });
 
+    item = "<li><a href='/'><img src='http://54.210.13.122/files/square_thumbnails/a008acc6a3c11b1fefe5d93ab888e59a.jpg' alt='Greene001.jpg' title='Greene001.jpg'></a></li>";
+
     jQuery('#jcarousel-about ul')
-        .append("<li>" + "one")
-        .append("<li>" + "two")
-        .append("<li>" + "three")
-        .append("<li>" + "one")
-        .append("<li>" + "two")
-        .append("<li>" + "three")
-        .append("<li>" + "one")
-        .append("<li>" + "two")
-        .append("<li>" + "three")
-        .append("<li>" + "one")
-        .append("<li>" + "two")
-        .append("<li>" + "three")
-        .append("<li>" + "one")
-        .append("<li>" + "two")
-        .append("<li>" + "three")
-        .append("<li>" + "one")
-        .append("<li>" + "two")
-        .append("<li>" + "three")
-        .append("<li>" + "one")
-        .append("<li>" + "two")
-        .append("<li>" + "three");
+        .append(item)
+        .append(item)
+        .append(item)
+        .append(item)
+        .append(item)
+        .append(item)
+        .append(item)
+        .append(item);
 
     console.log('fire');
 
