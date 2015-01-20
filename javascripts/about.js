cdb2ef97b3b52e7a736c85eea815c063a812fcff
Nabil Kashyap <n@nabilk.com>
2015-01-19 23:21:25 -0500

    about js tester

--- javascripts/about.js
+++ javascripts/about.js
@@ -3,8 +3,13 @@
 jQuery(function(){
 
     jQuery('.jcarousel').jcarousel({
-
+        center: true,
     });
+
+    jQuery('.jcarousel')
+        .append("<li>" + "one")
+        .append("<li>" + "two")
+        .append("<li>" + "three");
 
     console.log('fire');
 
