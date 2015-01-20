2a79343abb23446d1bb743f7c445f36d81a577a2
Nabil Kashyap <n@nabilk.com>
2015-01-19 23:51:32 -0500

    commented out about.js

--- javascripts/about.js
+++ javascripts/about.js
@@ -1,5 +1,6 @@
 // about page carousel
 
+/* 
 jQuery(function(){
 
     item = "<li><a href='/'><img src='http://54.210.13.122/files/square_thumbnails/a008acc6a3c11b1fefe5d93ab888e59a.jpg' alt='Greene001.jpg' title='Greene001.jpg'>Hello Now<p>Hello Now</p><h2>Hello Now</h2></a></li>";
@@ -19,3 +20,5 @@ jQuery(function(){
         
     });
 });
+
+*/
