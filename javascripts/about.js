f42405d035724eff6d61478e618e35a8b9899504
Nabil Kashyap <n@nabilk.com>
2015-01-20 16:37:38 -0500

    updated about.js to use new exhibits-json data syntax error

--- javascripts/about.js
+++ javascripts/about.js
@@ -8,7 +8,7 @@ jQuery(function(){
     for(i in exhibitsJSON){
 
         exhibit = "<li><a href='" + exhibitsJSON[i].url + "'>";
-        exhibit += "<img src='http://54.210.13.122/fullsize/square_thumbnails/";
+        exhibit += "<img src='http://54.210.13.122/files/fullsize/";
         exhibit += exhibitsJSON[i].thumb + "' title='" + exhibitsJSON[i].title + "'/>";
         exhibit += "<div><h3>" + exhibitsJSON[i].title + "</h3></a>";
         exhibit += "<p>" + exhibitsJSON[i].description + "</p></div></li>"
