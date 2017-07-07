007acf21330508b0fb328a58d230bc963702b503
bulbil <n@nabilk.com>
2017-07-06 20:11:40 -0400

    updates sass and js to reflect s3 urls

--- javascripts/exhibits-json.js
+++ javascripts/exhibits-json.js
@@ -4,73 +4,73 @@ exhibitsJSON =
     "title": "The Struggle for Black Studies",
     "description": "Documents related to the birth of the Black Studies program at Swarthmore.",
     "url": "/exhibits/show/black-studies",
-    "thumb": "fullsize/8dd5fc5d5a576b1f4e0ad09cd9e9e373.jpg"
+    "thumb": "fullsize%2F8dd5fc5d5a576b1f4e0ad09cd9e9e373.jpg"
   },
   {
     "title": "Faculty & Alumni Interviews",
     "description": "An exhibit of the interview materials collected during this project, including audio and video files, transcripts, and photographs.",
     "url": "/interviews",
-    "thumb": "fullsize/2ceef6c92d074a4a851ec6ce3b13f524.jpg"
+    "thumb": "fullsize%2F2ceef6c92d074a4a851ec6ce3b13f524.jpg"
   },
   {
     "title": "The Push for a Black Cultural Center",
     "description": "Documents relating to the acquisition of the Black Cultural Center located in Robinson House.",
     "url": "/exhibits/show/bcc",
-    "thumb": "fullsize/de58a225e176c328c42d593f1e451573.jpg"
+    "thumb": "fullsize%2Fde58a225e176c328c42d593f1e451573.jpg"
   },
   {
     "title": "Black Life at Swarthmore",
     "description": "An exhibit of the ordinary lives of Black students at Swarthmore, told through photographs.",
     "url": "/exhibits/show/black-life",
-    "thumb": "fullsize/6f43677f44b827df2a39077825a02ab3.jpg"
+    "thumb": "fullsize%2F6f43677f44b827df2a39077825a02ab3.jpg"
   },
   {
     "title": "The 1969 Sit-In",
     "description": "Documents and articles relating to the January 1969 SASS occupation of the Admissions Office.",
     "url": "/exhibits/show/1969-sit-in",
-    "thumb": "fullsize/032b1784bb8cf6b5acf690d401529b41.jpg"
+    "thumb": "fullsize%2F032b1784bb8cf6b5acf690d401529b41.jpg"
   },
   {
     "title": "Black Community Struggles: Chester and Media",
     "description": "Documents on the political and racial dilemmas happening in the nearby communities of Media and Chester.",
     "url": "/exhibits/show/black-community-struggles",
-    "thumb": "fullsize/5d60c6172050f6d7bc466acc63377e1c.jpg"
+    "thumb": "fullsize%2F5d60c6172050f6d7bc466acc63377e1c.jpg"
   },
   {
     "title": "Swarthmore & the Rockefeller Fund",
     "description": "Documents regarding Swarthmore's relationship to the Rockefeller Fund, a non-profit grant given to help increase Black enrollment.",
     "url": "/exhibits/show/swarthmore-rockefeller",
-    "thumb": "fullsize/26c21babf3aa8e86ad619695f55c673d.jpg"
+    "thumb": "fullsize%2F26c21babf3aa8e86ad619695f55c673d.jpg"
   },
   {
     "title": "Narratives of Black Student Protest at Swarthmore College",
     "description": "Histories of instances of Black student protest at the College, published in and collected from various media over the years.",
     "url": "/exhibits/show/narratives",
-    "thumb": "fullsize/0957f5e48137cbe3a996fa0b50166b88.jpg"
+    "thumb": "fullsize%2F0957f5e48137cbe3a996fa0b50166b88.jpg"
   },
   {
     "title": "Federal Bureau of Investigation",
     "description": "Documents from the FBI investigation into Black student protest in 1969.",
     "url": "/exhibits/show/fbi",
-    "thumb": "fullsize/9bd965a31b181abd660bac7615162cdb.jpg"
+    "thumb": "fullsize%2F9bd965a31b181abd660bac7615162cdb.jpg"
   },
   {
     "title": "Black Liberation 1969: The Soundtrack",
     "description": "A jukebox of twenty-five songs Swarthmore alumni remembered in interviews or mentioned in archival documents.",
     "url": "/soundtrack",
-    "thumb": "fullsize/efa1b13441d7da72e3acafcae3e36290.jpg"
+    "thumb": "fullsize%2Fefa1b13441d7da72e3acafcae3e36290.jpg"
   },
   {
     "title": "Black Student Enrollment",
     "description": "A visualization of enrollment data provided by the Swarthmore Alumni Relations Office, consisting of the number of graduated and non-graduated alumni who associate with each class year.",
     "url": "/enrollment",
-    "thumb": "fullsize/enrollment_viz.png"
+    "thumb": "fullsize%2Fenrollment_viz.png"
   },
   {
     "title": "Student Photographs",
     "description": "Photographs identifying many of the key alumni involved in the events related to the black student movement at Swarthmore 1968-1972.",
     "url": "/photos",
-    "thumb": "fullsize/a008acc6a3c11b1fefe5d93ab888e59a.jpg"
+    "thumb": "fullsize%2Fa008acc6a3c11b1fefe5d93ab888e59a.jpg"
   },
   {
     "title": "Mapping the 1969 Sit-in",
--- javascripts/main-carousel.js
+++ javascripts/main-carousel.js
@@ -3,12 +3,14 @@
  
 jQuery(function(){
 
+   var s3Prefix = 'http://s3.amazonaws.com/sc-lib-ds-bl1969%2F';
    var carousel = jQuery('.jcarousel#jcarousel-main ul');
 
     for(i in exhibitsJSON){
 
         var exhibit = "<li><a href='" + exhibitsJSON[i].url + "'>";
-        exhibit += "<div style='background: url(../files/";
+        exhibit += "<div style='background: url(";
+        exhibit += s3Prefix;
         exhibit += exhibitsJSON[i].thumb + ")'>";
         exhibit += "<div><h2>" + exhibitsJSON[i].title + "</h2>";
         exhibit += "<p>" + exhibitsJSON[i].description + "</p></div></div></li>"
